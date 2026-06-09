edfd420b7e6402d20e43887331dd6251ad9f06c1
ANKUSH0480 <rajankush0480@gmail.com>
2026-06-09 17:58:13 +0530

    SOUT

--- BASICS OF JAVA/InputOutput.java
+++ BASICS OF JAVA/InputOutput.java
@@ -1,3 +1,54 @@
+// 1- LIBRARY provide pre - built classes and methods to perform various task 
+
+import java.util.Scanner; // Scanner is a class in java.util 
+// package used to take input from user
+
 public class InputOutput {
-    
-}
+    public static void main(String[] args) { 
+    }
+       
+} 
+
+//  2- TO PRINT OUTPUT ON THE SCREEN 
+
+public class InputOutput{
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // println is a method used to 
+        // print output on the screen 
+    }
+} 
+
+ // output - Hello World 
+
+// 3- PRINTING MULTIPLE LINES 
+
+public class InputOutput{
+
+    public static void main(String[] args) {
+
+        System.out.println("ANKUSH ");
+
+        System.out.println("ANKUSH");
+
+    }
+
+} 
+
+//OUTPUT - ANKUSH 
+//          ANKUSH
+
+// WE CAN ALSO USE \n TO PRINT MULTIPLE LINES
+
+
+    public class InputOutput{
+
+    public static void main(String[] args) {
+
+        System.out.print("ANKUSH\n");
+
+        System.out.print("ANKUSH");
+
+    }
+
+} 
+
