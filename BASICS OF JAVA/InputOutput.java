e462e697fa15b5945d2056328baba2c42de194a8
ANKUSH0480 <rajankush0480@gmail.com>
2026-06-09 18:01:05 +0530

    TAKING THE INPUT

--- BASICS OF JAVA/InputOutput.java
+++ BASICS OF JAVA/InputOutput.java
@@ -1,54 +1,72 @@
-// 1- LIBRARY provide pre - built classes and methods to perform various task 
+// // 1- LIBRARY provide pre - built classes and methods to perform various task 
 
-import java.util.Scanner; // Scanner is a class in java.util 
-// package used to take input from user
+// import java.util.Scanner; // Scanner is a class in java.util 
+// // package used to take input from user
+
+// public class InputOutput {
+//     public static void main(String[] args) { 
+//     }
+       
+// } 
+
+// //  2- TO PRINT OUTPUT ON THE SCREEN 
+
+// public class InputOutput{
+//     public static void main(String[] args) {
+//         System.out.println("Hello World"); // println is a method used to 
+//         // print output on the screen 
+//     }
+// } 
+
+//  // output - Hello World 
+
+// // 3- PRINTING MULTIPLE LINES 
+
+// public class InputOutput{
+
+//     public static void main(String[] args) {
+
+//         System.out.println("ANKUSH ");
+
+//         System.out.println("ANKUSH");
+
+//     }
+
+// } 
+
+// //OUTPUT - ANKUSH 
+// //          ANKUSH
+
+// // WE CAN ALSO USE \n TO PRINT MULTIPLE LINES
+
+
+//     public class InputOutput{
+
+//     public static void main(String[] args) {
+
+//         System.out.print("ANKUSH\n");
+
+//         System.out.print("ANKUSH");
+
+//     }
+
+// } 
+
+// 4-- TAKING USER INPUT WITH SCANNER 
+
+        import java.util.Scanner;
 
 public class InputOutput {
-    public static void main(String[] args) { 
-    }
-       
-} 
-
-//  2- TO PRINT OUTPUT ON THE SCREEN 
-
-public class InputOutput{
     public static void main(String[] args) {
-        System.out.println("Hello World"); // println is a method used to 
-        // print output on the screen 
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        System.out.println("Value of x: " + x);
+
+        sc.close();
     }
 } 
 
- // output - Hello World 
-
-// 3- PRINTING MULTIPLE LINES 
-
-public class InputOutput{
-
-    public static void main(String[] args) {
-
-        System.out.println("ANKUSH ");
-
-        System.out.println("ANKUSH");
-
-    }
-
-} 
-
-//OUTPUT - ANKUSH 
-//          ANKUSH
-
-// WE CAN ALSO USE \n TO PRINT MULTIPLE LINES
-
-
-    public class InputOutput{
-
-    public static void main(String[] args) {
-
-        System.out.print("ANKUSH\n");
-
-        System.out.print("ANKUSH");
-
-    }
-
-} 
-
+// TAKING THE INPUT = 10 
+// OUTPUT = Value of x: 10
