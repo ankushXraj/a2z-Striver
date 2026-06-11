d1906e2448d8cd55b40a1519d91ca320460a07f0
ANKUSH0480 <rajankush0480@gmail.com>
2026-06-11 14:53:29 +0530

    ARRAYS

---
+++ BASICS OF JAVA/Arrays.java
@@ -0,0 +1,30 @@
+// int[] myArray = new int[8]; // Syntax to create an array of integers with 8 elements
+ 
+
+// int[] arr = {10, 20, 30, 40, 50}; // Initializing an Array
+
+
+//  import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) { 
+        System.out.println("aman  ");
+
+//    Scanner sc = new Scanner(System.in);
+
+//         int n = sc.nextInt();
+
+//      int[] arr = new int[n];
+
+//         for(int i = 0; i < n; i++) {
+//             arr[i] = sc.nextInt();
+//         }
+//         for(int i = 0; i < n; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//         sc.close();
+    }
+} 
+
+//          INPUT -  5
+// 10 20 30 40 50
---
+++ BASICS OF JAVA/tempCodeRunnerFile.java
@@ -0,0 +1,4 @@
+int[] myArray = new int[8]; // Syntax to create an array of integers with 8 elements
+ 
+
+// int[] arr = {10, 20, 30, 40, 50}; // Initializing an Array
