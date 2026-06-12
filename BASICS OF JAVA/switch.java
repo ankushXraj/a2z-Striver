0601c7af5b84ff91088ced542eb2f9eedc19c587
ANKUSH0480 <rajankush0480@gmail.com>
2026-06-12 19:10:24 +0530

    SWITCH

--- BASICS OF JAVA/switch.java
+++
@@ -1,7 +0,0 @@
-//  A switch statement is used when we want to compare one variable with multiple fixed values.
-
-// Instead of writing many if-else if conditions, we can use switch to make the code cleaner and easier to read.
- 
- public class switch {
-    
-}
